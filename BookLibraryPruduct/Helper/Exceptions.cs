ab919af2f0728bc9f21db877952dafa09a0f6052
CavadovT <turalkhj@code.edu.az>
2022-04-03 20:32:13 +0400

    Add project files.

---
+++ BookLibraryPruduct/Helper/Exceptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookLibraryPruduct.Helper
+{
+    static class Exceptions
+    {
+        /*
+        Aşağıdakı custom exceptionları yaradın:
+            - CapacityLimitException
+            - NotFoundException
+            - ProductCountIsZeroException
+
+         */
+        public static void CapacityLimitException(int capacity, int count) 
+        {
+            if (capacity >= count) 
+            {
+                throw new Exception("capacity was less than the current size. (Parameter 'value')");
+            }
+        }
+    }
+}
---
+++ BookLibraryPruduct/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookLibraryPruduct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region test 
+            //try
+            //{
+            //    List<int> list = new List<int>();
+            //    list.Add(1);
+            //    list.Capacity = 0;
+            //    Console.WriteLine(list.Capacity);
+                
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+            //}
+            #endregion
+        }
+    }
+}
