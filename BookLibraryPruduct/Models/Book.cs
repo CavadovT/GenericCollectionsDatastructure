a4ae6b1e4aef69a2a162539603463804d0c57e8b
CavadovT <turalkhj@code.edu.az>
2022-04-04 06:25:58 +0400

    feat: creat librabry classs

--- BookLibraryPruduct/Models/Book.cs
+++ BookLibraryPruduct/Models/Book.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookLibraryPruduct.Helper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,24 +7,51 @@ namespace BookLibraryPruduct.Models
 {
      class Book:Product
     {
-                /*
-                 Book class (Product-dan miras alır)
-        - AuthorName
-        - PageCount
-        
-        ShowInfo methodunu override edir və bütün book-un məlumatlarını console-a çıxardır.
-        Sell methodunu override edir və 
-                hər dəfə sell methodu işə düşəndə bir ədəd book satılmalıdır 
-                count azalır TotalInCome isə artır, 
-                əgər Product-ın count-ı sıfır olarsa o zaman ProductCountIsZeroException baş versin və 
-                satış baş verməsin.
-        
-        ps: name, price, authorName, pageCount dəyərləri olmadan book obyekti yaratmaq olmaz.
-        */
-        
-        public Book(string name)
+        /*
+         Book class (Product-dan miras alır)
+- AuthorName
+- PageCount
+
+ShowInfo methodunu override edir və bütün book-un məlumatlarını console-a çıxardır.
+Sell methodunu override edir və 
+        hər dəfə sell methodu işə düşəndə bir ədəd book satılmalıdır 
+        count azalır TotalInCome isə artır, 
+        əgər Product-ın count-ı sıfır olarsa o zaman ProductCountIsZeroException baş versin və 
+        satış baş verməsin.
+
+ps: name, price, authorName, pageCount dəyərləri olmadan book obyekti yaratmaq olmaz.
+*/
+
+        public string AuthorName { get; set; }
+        public int PageCount { get; set; }
+        public Book(string name,string authorName,int price,int pageCount)
+        {
+            Name = name;
+            AuthorName = authorName;
+            Price = price;
+            PageCount = pageCount;
+
+        }
+        public override void Sell()
+        {
+            Exceptions.ProductCountIsZeroException(Count);
+            Count--;
+            TotalInCome += Price*30/100.0;
+        }
+
+        public override void ShowInfo()
         {
            
+            Console.Write($"\nProduct Id:{ID}\n Book Name: {Name}\n Aouthor Name: {AuthorName}\n" +
+                $"Page Count: {PageCount}\nPrice: {Price}\nCount in base: {Count}" +
+                $"TotalInCome: {TotalInCome}\n");
+            Console.WriteLine("+++++++++++++++++++++++++");
+        }
+        public override string ToString()
+        {
+            return $"\nProduct Id:{ID}\n Book Name: {Name}\n Aouthor Name: {AuthorName}\n" +
+                $"Page Count: {PageCount}\nPrice: {Price}\nCount in base: {Count}" +
+                $"Total In Come : {TotalInCome}\n" + "++++++++++++++++";
         }
     }
 }
--- BookLibraryPruduct/Models/Library.cs
+++ BookLibraryPruduct/Models/Library.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BookLibraryPruduct.Helper;
 
 namespace BookLibraryPruduct.Models
 {
      class Library
     {
+        /*
+         * Library class
+             - BookLimit
+            - Books - List olacaq
+            - AddBook() - Parametr olaraq bir Book obyekti qəbul edir və 
+        books listinə əlavə edir əgər
+        BookLimiti aşırsa geriyə CapacityLimitException qaytarsın
+            - GetBookById() - Parametr olaraq nullable bir id qəbul edir
+        və o Book obyektini tapıb geriyə qaytarır 
+        əgər id null olarsa geriyə NullReferenceException qaytarsın ümumiyyətlə
+        göndərilən id-li book-u tapmasa geriyə null qaytarsın.
+            - RemoveById() - Parametr olaraq nullable bir id qəbul edir 
+        və o Book obyektini tapıb Books listindən silir
+        əgər id null olarsa geriyə NullReferenceException qaytarsın 
+        ümumiyyətlə göndərilən id-li book-u tapmasa geriyə NotFoundException qaytarsın.
+            
+            
+            ps: BookLimit olmadan Library obyekti yaratmaq olmaz.
+         
+         */
+        public int BookLimit { get; set; }
+        public int Count { get; set; }
+        
         List<Book> books = new List<Book>();
+        public Library(int bookLimit)
+        {
+            BookLimit = bookLimit;
+           
+        }
+        public void AddBook(Book book) 
+        {
+        if (Count==BookLimit)
+        }
         public void GetBookByID(int? id) 
         {  
             if (id != null) 
